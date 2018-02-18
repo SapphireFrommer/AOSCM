807525d855448577653dd072fcc1b4b019ebe760
Hanan <marinbh@biu.ac.il>
2018-02-18 20:54:51 +0200

    shorter dict name

--- standard_cell.py
+++ standard_cell.py
@@ -2,71 +2,71 @@
 from salamandra import *
 from define_parameters import *
 
-standard_cell = {}
+sc = {}     # standard_cell
 
 if param['technology'] == 'tsmc65':
-    standard_cell['latch'] = {}
-    standard_cell['latch']['component'] = Component('LATQ_X1M_A12TR')
-    standard_cell['latch']['in'] = 'D'
-    standard_cell['latch']['component'].add_pin(Input(standard_cell['latch']['in']))
-    standard_cell['latch']['clk'] = 'G'     
-    standard_cell['latch']['component'].add_pin(Input(standard_cell['latch']['clk']))
-    standard_cell['latch']['out'] = 'Q'    
-    standard_cell['latch']['component'].add_pin(Output(standard_cell['latch']['out']))
+    sc['latch'] = {}
+    sc['latch']['component'] = Component('LATQ_X1M_A12TR')
+    sc['latch']['in'] = 'D'
+    sc['latch']['component'].add_pin(Input(sc['latch']['in']))
+    sc['latch']['clk'] = 'G'     
+    sc['latch']['component'].add_pin(Input(sc['latch']['clk']))
+    sc['latch']['out'] = 'Q'    
+    sc['latch']['component'].add_pin(Output(sc['latch']['out']))
 
-    standard_cell['flipflop'] = {}
-    standard_cell['flipflop']['component'] = Component('DFFQ_X2M_A12TR')
-    standard_cell['flipflop']['in'] = 'D'
-    standard_cell['flipflop']['component'].add_pin(Input(standard_cell['flipflop']['in'])) 
-    standard_cell['flipflop']['clk'] = 'CK'
-    standard_cell['flipflop']['component'].add_pin(Input(standard_cell['flipflop']['clk']))
-    standard_cell['flipflop']['out'] = 'Q'
-    standard_cell['flipflop']['component'].add_pin(Output(standard_cell['flipflop']['out']))    
+    sc['flipflop'] = {}
+    sc['flipflop']['component'] = Component('DFFQ_X2M_A12TR')
+    sc['flipflop']['in'] = 'D'
+    sc['flipflop']['component'].add_pin(Input(sc['flipflop']['in'])) 
+    sc['flipflop']['clk'] = 'CK'
+    sc['flipflop']['component'].add_pin(Input(sc['flipflop']['clk']))
+    sc['flipflop']['out'] = 'Q'
+    sc['flipflop']['component'].add_pin(Output(sc['flipflop']['out']))    
     
-    standard_cell['buffer'] = {}
-    standard_cell['buffer']['component'] = Component('BUFH_X3M_A12TR')
-    standard_cell['buffer']['in'] = 'A'
-    standard_cell['buffer']['component'].add_pin(Input(standard_cell['buffer']['in']))    
-    standard_cell['buffer']['out'] = 'Y'
-    standard_cell['buffer']['component'].add_pin(Output(standard_cell['buffer']['out']))    
+    sc['buffer'] = {}
+    sc['buffer']['component'] = Component('BUFH_X3M_A12TR')
+    sc['buffer']['in'] = 'A'
+    sc['buffer']['component'].add_pin(Input(sc['buffer']['in']))    
+    sc['buffer']['out'] = 'Y'
+    sc['buffer']['component'].add_pin(Output(sc['buffer']['out']))    
 
-    standard_cell['inverter'] = {}
-    standard_cell['inverter']['component'] = Component('INV_X1B_A12TR')
-    standard_cell['inverter']['in'] = 'A'
-    standard_cell['inverter']['component'].add_pin(Input(standard_cell['inverter']['in']))
-    standard_cell['inverter']['out'] = 'Y'
-    standard_cell['inverter']['component'].add_pin(Input(standard_cell['inverter']['out']))
+    sc['inverter'] = {}
+    sc['inverter']['component'] = Component('INV_X1B_A12TR')
+    sc['inverter']['in'] = 'A'
+    sc['inverter']['component'].add_pin(Input(sc['inverter']['in']))
+    sc['inverter']['out'] = 'Y'
+    sc['inverter']['component'].add_pin(Input(sc['inverter']['out']))
 
-    standard_cell['NAND2'] = {}
-    standard_cell['NAND2']['component'] = Component('NAND2_X1A_A12TR')
-    standard_cell['NAND2']['in_1'] = 'A'
-    standard_cell['NAND2']['component'].add_pin(Input(standard_cell['NAND2']['in_1']))  
-    standard_cell['NAND2']['in_2'] = 'B'
-    standard_cell['NAND2']['component'].add_pin(Input(standard_cell['NAND2']['in_2'])) 
-    standard_cell['NAND2']['out'] = 'Y'   
-    standard_cell['NAND2']['component'].add_pin(Output(standard_cell['NAND2']['out']))    
+    sc['NAND2'] = {}
+    sc['NAND2']['component'] = Component('NAND2_X1A_A12TR')
+    sc['NAND2']['in_1'] = 'A'
+    sc['NAND2']['component'].add_pin(Input(sc['NAND2']['in_1']))  
+    sc['NAND2']['in_2'] = 'B'
+    sc['NAND2']['component'].add_pin(Input(sc['NAND2']['in_2'])) 
+    sc['NAND2']['out'] = 'Y'   
+    sc['NAND2']['component'].add_pin(Output(sc['NAND2']['out']))    
 
-    standard_cell['NOR2'] = {}
-    standard_cell['NOR2']['component'] = Component('NOR2_X1A_A12TR')
-    standard_cell['NOR2']['in_1'] = 'A'
-    standard_cell['NOR2']['component'].add_pin(Input(standard_cell['NOR2']['in_1']))    
-    standard_cell['NOR2']['in_2'] = 'B'
-    standard_cell['NOR2']['component'].add_pin(Input(standard_cell['NOR2']['in_2']))   
-    standard_cell['NOR2']['out'] = 'Y'    
-    standard_cell['NOR2']['component'].add_pin(Output(standard_cell['NOR2']['out']))    
+    sc['NOR2'] = {}
+    sc['NOR2']['component'] = Component('NOR2_X1A_A12TR')
+    sc['NOR2']['in_1'] = 'A'
+    sc['NOR2']['component'].add_pin(Input(sc['NOR2']['in_1']))    
+    sc['NOR2']['in_2'] = 'B'
+    sc['NOR2']['component'].add_pin(Input(sc['NOR2']['in_2']))   
+    sc['NOR2']['out'] = 'Y'    
+    sc['NOR2']['component'].add_pin(Output(sc['NOR2']['out']))    
 
-    standard_cell['well_tap'] = Component('WELLANTENNATIEPW2_A12TR')
+    sc['well_tap'] = Component('WELLANTENNATIEPW2_A12TR')
 
-    standard_cell['latch_clock_gate'] = {}
-    standard_cell['latch_clock_gate']['component'] = Component('PREICG_X0P5B_A12TR')
-    standard_cell['latch_clock_gate']['clk'] = 'CK'
-    standard_cell['latch_clock_gate']['component'].add_pin(Input(standard_cell['latch_clock_gate']['clk']))    
-    standard_cell['latch_clock_gate']['E'] = 'E'
-    standard_cell['latch_clock_gate']['component'].add_pin(Input(standard_cell['latch_clock_gate']['E']))    
-    standard_cell['latch_clock_gate']['SE'] = 'SE'
-    standard_cell['latch_clock_gate']['component'].add_pin(Input(standard_cell['latch_clock_gate']['SE']))    
-    standard_cell['latch_clock_gate']['out'] = 'ECK'
-    standard_cell['latch_clock_gate']['component'].add_pin(Output(standard_cell['latch_clock_gate']['out']))    
+    sc['latch_clock_gate'] = {}
+    sc['latch_clock_gate']['component'] = Component('PREICG_X0P5B_A12TR')
+    sc['latch_clock_gate']['clk'] = 'CK'
+    sc['latch_clock_gate']['component'].add_pin(Input(sc['latch_clock_gate']['clk']))    
+    sc['latch_clock_gate']['E'] = 'E'
+    sc['latch_clock_gate']['component'].add_pin(Input(sc['latch_clock_gate']['E']))    
+    sc['latch_clock_gate']['SE'] = 'SE'
+    sc['latch_clock_gate']['component'].add_pin(Input(sc['latch_clock_gate']['SE']))    
+    sc['latch_clock_gate']['out'] = 'ECK'
+    sc['latch_clock_gate']['component'].add_pin(Output(sc['latch_clock_gate']['out']))    
 
 else:
     pass
