7019451599f894e51098f54cfff05997397ab99d
Hanan <marinbh@biu.ac.il>
2018-06-26 17:27:59 +0300

    rm dict

--- verilog_modules/define.v
+++
@@ -1,7 +0,0 @@
-//####################### define parameter #######################
-`define ADDR_WIDTH 8
-`define DATA_WIDTH 16
-`define RWL_NUM 4
-`define TOPLEVEL scm65
-`define WELLTAP 8
-`define technology tsmc65
