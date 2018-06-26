3db402e51327f50be6d247e7041b4d8a79b5cb27
Hanan <marinbh@biu.ac.il>
2018-06-26 17:59:19 +0300

    rm dict

--- TB/MidGap_DGWCLK/MidGap_DGWCLK_tb.f
+++
@@ -1,7 +0,0 @@
-../../verilog_modules/define.v
-
-MidGap_DGWCLK_tb.v
-../../verilog_modules/MidGap_DGWCLK/MidGap_DGWCLK.v
-
--v /data/tsmc/65LP/dig_libs/ARM_FEONLY/arm/tsmc/cln65lp/sc12_base_rvt/r0p0/verilog/sc12_cln65lp_base_rvt.v
--v /data/tsmc/65LP/dig_libs/ARM_FEONLY/arm/tsmc/cln65lp/sc12_base_rvt/r0p0/verilog/sc12_cln65lp_base_rvt_udp.v
