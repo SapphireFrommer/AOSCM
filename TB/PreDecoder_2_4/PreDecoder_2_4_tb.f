7c948f084f9359b546c67740b9e2c98d14fe9480
Hanan <marinbh@biu.ac.il>
2018-06-26 17:59:57 +0300

    add dict

---
+++ TB/PreDecoder_2_4/PreDecoder_2_4_tb.f
@@ -0,0 +1,4 @@
+PreDecoder_2_4_tb.v
+../../verilog_modules/PreDecoder_2_4/PreDecoder_2_4.v
+-v /data/tsmc/65LP/dig_libs/ARM_FEONLY/arm/tsmc/cln65lp/sc12_base_rvt/r0p0/verilog/sc12_cln65lp_base_rvt.v
+-v /data/tsmc/65LP/dig_libs/ARM_FEONLY/arm/tsmc/cln65lp/sc12_base_rvt/r0p0/verilog/sc12_cln65lp_base_rvt_udp.v
---
+++ TB/rwlBuff_strip/rwlBuff_stripe_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns/100ps
+
+module rwlBuff_strip_tb;
+	//reg clk;
+    reg [2**`ADDR_WIDTH-1:0] in;
+    wire [2**`ADDR_WIDTH-1:0] out;
+
+	rwlBuff_stripe UUT(.IN(in), .OUT(out));
+
+	//always #1 Clk=~Clk;
+
+	initial
+		begin
+			$display("\t\ttime:\tIN\t\tOUT");
+			$monitor($time,":\t%b\t%b",in,out);
+            
+            in = 8'b0000_0001;
+            #2
+            in = 8'b0100_0001;
+
+			#2
+            in = 8'b0110_0001;
+
+        	#1
+            in = 8'b1001_0010;
+
+			#4 $finish;			
+		end
+endmodule
+
