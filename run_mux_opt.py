be4aaeed0d4b01bbb68c82ba611e44ad965a8d1f
Sapphire <sapphire.frommer@live.biu.ac.il>
2021-12-26 03:54:29 +0200

    run mux updated for int only

--- run_mux_opt.py
+++ run_mux_opt.py
@@ -111,7 +111,7 @@ for i in range(level_var**level):
 
     runPaths['workspace'] = runPaths['home'] + 'workspace/'
     runPaths['PLACEANDROUTE'] = runPaths['workspace'] + "innovus/"
-    runCommands['PLACEANDROUTE'] = runCommands['SGE'] + 'qinnovus -files ../../innovus/backend.tcl \''
+    runCommands['PLACEANDROUTE'] = runCommands['SGE'] + 'innovus -files ../../innovus/backend.tcl \''
               
     os.system("cd " + runPaths['PLACEANDROUTE'] + " && "+ runCommands['PLACEANDROUTE'])
     print('DONE - RUN INNOVUS \n')
@@ -120,190 +120,49 @@ for i in range(level_var**level):
     ##                          add data to summery file                             ##
     ###################################################################################
     
-    # ###check if an old version exists, and if so, overwrite.
-    # existed = 0
+    #id name = str(power)
+    #width from width.rpt
+    fin = open("./width.rpt","r")
+    width = fin.readline()
+    fin.close()
+    #width = 7 #for now
+    #delay time = from reports/timing_summary.rpt
+    fin = open("./reports/timing_summary.rpt","r")
+    delay = fin.readline()
+    fin.close()
+    #delay = 5 #for now
+    
+    found = False
+    fin = open("./summary.rpt", "r")
+    new_file_lines = []
+    for line in fin:
+      if (str(power) in line):
+        new_file_lines.append('ID: '+str(power)+'\n')
+        new_file_lines.append('WIDTH: '+str(width)+'\n')
+        new_file_lines.append(str(delay)+'\n')
+        #mark as found
+        found = True
+        #skip the other two lines we override
+        line = next(fin)
+        line = next(fin)
+      else:
+        new_file_lines.append(line)
+        line = next(fin)
+        new_file_lines.append(line)
+        line = next(fin)
+        new_file_lines.append(line)
+    #if wasn't found in the file, add it
+    if not found:
+      new_file_lines.append('ID: '+str(power)+'\n')
+      new_file_lines.append('WIDTH: '+str(width)+'\n')
+      new_file_lines.append('DELAY TIME: '+str(delay)+'\n')
+      
 
-    # summery_data = str(RUN_ID_NAME)+' @  Total delay: '+'timing report name'+' @ Width: '+'tcl_FloorPlan_file_name'+'\n'
-    # fin = open("./summery.rpt", "r")
-    # new_file_lines = []
-    # for line in fin:
-    #   if (str(RUN_ID_NAME) in line):
-    #     existed = 1
-    #     new_file_lines.append(summery_data)
-    #   else:
-    #     new_file_lines.append(line)
-    # if(not existed)
-    #   new_file_lines.append(summery_data)
-
-    # fin.close()
-    # fout = open("./summery.rpt", "w")
-    # for line in new_file_lines:
-    #     fout.write(line)
-    # fout.close()
-
-
-#         
+    fin.close()
+    fout = open("./summary.rpt", "w")
+    for line in new_file_lines:
+      fout.write(line)
+    fout.close() 
+            
+          
 exit()
-
-    
-    
-"""   
-     
-for ROWS in ROWS_LIST:
-  #for ADDR_WIDTH in ADDR_WIDTH_LIST:
-   for DATA_WIDTH in DATA_WIDTH_LIST:
-    for WP_RP_TUPLE in WP_RP_TUPLE_list:
-      WP = WP_RP_TUPLE[0]
-      RP = WP_RP_TUPLE[1]
-    #for WP in WP_LIST:
-     #for RP in RP_LIST:
-      for CLK_PERIOD in CLK_PERIODS_LIST:
-       for RUNTYPE in RUNTYPES:
-        # ---- Run Commands ---- #  
-        runCommands['setVariables']=' "set RUNTYPE '+RUNTYPE+'" '
-        runCommands['XTERM'] = runCommands['SGE'] + 'xterm'
-        runCommands['TB_RTL']  = ''
-  
-        #runCommands['SYNTHESIS']  = 'rc -bg -execute ' + runCommands['setVariables'] +' -files scripts/synthesis.tcl \''
-        runCommands['SYNTHESIS']  = runCommands['SGE'] + 'xterm -e genus -bg -execute ' + runCommands['setVariables'] +' -files ../../scripts/genus.tcl \''
-        runCommands['MODIFY_mem_tb_mpscm_netlist_DUT']  = 'python modify_mem_tb_mpscm_netlist_DUT.py'     # MODIFY_mem_tb_mpscm_netlist_DUT (add all ports to DUT in TB)
-        runCommands['MODIFY_mpscm_postsyn_FOR_TB']  = 'python modify_mpscm.postsyn_for_tb.py'             # modify_mpscm.postsyn for tb (duplicate postsyn netlist and change toplaevel module name)
-        runCommands['MODIFY_pin_to_rtl']  = 'python modify_pin_to_rtl.py'                                 # modify_pin_to_rtl  (change pin placement to fit syn rtl)
-        runCommands['SALAMANDRA']  = runCommands['SGE']+'python3 mpscm_compiler_salamandra.py \''
-        runCommands['TB_SALAMANDRA']  = runCommands['SGE'] + 'xterm -e irun -f ../../sourcecode/TB/tb_file_list.f -debug -sv -top mem_tb -l irun_SALAMANDRA.log -timescale 1ns/1ps -nospecify -delta_sequdp_delay -define SALAMANDRA \''
-        runCommands['TB_NETLIST']  = runCommands['SGE'] + 'xterm -e irun -f ../../sourcecode/TB/tb_file_list.f -debug -sv -top mem_tb -l irun_POSTSYN.log -timescale 1ns/1ps -nospecify -delta_sequdp_delay -define POSTSYN \''
-        runCommands['PLACEANDROUTE'] = runCommands['SGE'] + 'xterm -e innovus -files ../../innovus/backend.tcl \''
-        #runCommands['PLACEANDROUTE'] = runCommands['SGE'] + 'xterm -e innovus -execute ' + runCommands['setVariables'] +' -files innovus/backend.tcl \''  
-        runCommands['MODIFY_mpscm_backannotation_FOR_TB']  = 'python modify_mpscm.backannotation_for_tb.py'   # modify_mpscm.backannotation for tb (change backannotation netlist module name and change TB DUT module name)  
-        #backAnnotationOptions = ' -access +rwc -64bit -timescale 1ns/1ps -define BACKANNOTATION -v ' + runPaths['standard_cell_verilog']
-        #runCommands['BACKANNOTATION'] = runCommands['SGE'] + 'xterm -e irun -f ../../sourcecode/TB/tb_file_list.f -debug -sv -top mem_tb -timescale 1ns/1ps -delta_sequdp_delay -define BACKANNOTATION -input ../../scripts/backannotation.tcl \''   # +sdf_verbose
-        runCommands['BACKANNOTATION'] = runCommands['SGE'] + 'xterm -e irun -f ../../sourcecode/TB/tb_file_list.f -debug -sv -disable_sem2009 -top mem_tb -l irun_BACKANNOTATION.log -timescale 1ns/1ps -define BACKANNOTATION -input ../../scripts/backannotation.tcl -sdf_simtime \''   # +sdf_verbose
-        runCommands['POWERREPORTS'] = runCommands['SGE'] + 'xterm -e innovus -files ../../innovus/create_power_reports.tcl \''
-     
-
-        # ----- Setup basic variables ----- #
-        #ROWS = 2**ADDR_WIDTH
-        ADDR_WIDTH = math.ceil(math.log2(ROWS))
-        sizeString=str(ROWS)+'X'+str(DATA_WIDTH)+'_WP_'+str(WP)+'_RP_'+str(RP)
-        runPaths['main'] = runPaths['home']+sizeString+'_CLK_'+str(CLK_PERIOD)+'/'+RUNTYPE+'/'
-        runPaths['SOURCE'] = runPaths['main'] + "sourcecode/"
-        runPaths['MODIFY_mem_tb_mpscm_netlist_DUT'] = runPaths['SOURCE']+'TB/'
-        runPaths['MODIFY_mpscm_postsyn_FOR_TB'] = runPaths['SOURCE']+'TB/'
-        runPaths['MODIFY_pin_to_rtl'] = runPaths['main']+'scripts/'
-        runPaths['workspace'] = runPaths['main'] + 'workspace/'
-        runPaths['SALAMANDRA'] = runPaths['main'] + 'salamandra/'
-        runPaths['TB_SALAMANDRA'] = runPaths['workspace'] + "TB/"                
-        runPaths['SYNTHESIS'] = runPaths['workspace'] + "synthesis/"
-        runPaths['TB_NETLIST'] = runPaths['workspace'] + "TB/"
-        runPaths['PLACEANDROUTE'] = runPaths['workspace'] + "innovus/"
-        runPaths['MODIFY_mpscm_backannotation_FOR_TB'] = runPaths['SOURCE']+'TB/'        
-        runPaths['BACKANNOTATION'] = runPaths['workspace'] + "TB/"
-        runPaths['POWERREPORTS'] = runPaths['workspace'] + "innovus/"
-        os.environ["DISPLAY"] = "enicsm01:"+str(MY_VNC)+".0"
-
-        # ----- Setup Run Directory ----- #
-        if SETUP_RUN_DIRECTORY:
-            if os.path.exists(runPaths['main']):
-                os.system("rm -r -f "+runPaths['main'])
-            print('Creating Run Directory: ' + runPaths['main'])
-            os.makedirs(runPaths['main'])
-            os.system("cp -r " + runPaths['skel'] + '/* ' + runPaths['main'])
-            pythonLogFile = open(runPaths['main'] + 'logFile.txt' ,'w')
-            pythonLogFile.close()
-            
-            # ----- Customize all Defines File ----- #
-            file_list = []          # tupple of: (source file -> destination file)           
-            file_list += [(runPaths['skel']+'salamandra/define_parameters.py', runPaths['main']+'salamandra/define_parameters.py')]
-
-            
-
-            for f in file_list:
-                f_src = open(f[0], 'r')
-                f_dst = open(f[1], 'w')
-                for line in f_src:
-                    if 'MUX_DRIVE_STRENGTH' in line:
-                      newLine = 'params['MUX_DRIVE_STRENGTH'] = [2,2,2,8,1,1,1]'                     
-                    
-
-                    f_dst.write(newLine)
-                f_src.close()
-                f_dst.close()
-
-        else:
-            pass
-            #pythonLogFile = open(runPaths['main'] + 'logFile.txt' ,'a')
-
-
-
-        runTimes['START'] = datetime.datetime.now().replace(microsecond=0)
-        for STAGE in STAGES_TO_RUN:
-            if (RUNTYPE != 'RTL'):
-                if (STAGE in ['MODIFY_pin_to_rtl','SYNTHESIS','MODIFY_mpscm_postsyn_FOR_TB','TB_NETLIST']):
-                    continue
-            if (RUNTYPE == 'RTL'):
-                if (STAGE in ['TB_SALAMANDRA']):
-                    continue
-            start_time = datetime.datetime.now().replace(microsecond=0)
-            pythonLogFile = open(runPaths['main'] + 'logFile.txt' ,'a')
-            pythonLogFile.write(STAGE + ' started at ' +str(start_time) + '\n')
-            print("cd " + runPaths[STAGE] + " && "+ runCommands[STAGE]+ '\n'+ '\n')
-            os.system("cd " + runPaths[STAGE] + " && "+ runCommands[STAGE])
-            end_time = datetime.datetime.now().replace(microsecond=0)
-            runTimes[STAGE]=end_time-start_time
-            pythonLogFile.write(STAGE + ' ended at ' + str(end_time) + '\n')
-            pythonLogFile.write(STAGE + ' took ' + str(runTimes[STAGE]) + '\n')
-            pythonLogFile.close()
-    
-        runTimes['END'] = datetime.datetime.now().replace(microsecond=0)
-        runTimes['TOTAL'] = runTimes['END']-runTimes['START']
-
-        ##########################################################
-        #################### Write to summaryFile #################
-        ##########################################################
-
-        if CREATE_REPORTS:
-            print('\n'+'#'*40)
-            print('START CREATE REPORTS (summaryFile.txt)')
-            print('#'*40+'\n')
-            reportsPath = runPaths['main']+'reports/'
-            innoPath = reportsPath+'innovus/'
-            PowerReportsPath = reportsPath+'innovus/power_reports/'
-
-            summaryFile = open(runPaths['main']+ 'summaryFile.txt' ,'a') 
-            summaryFile.write('Summary file of - '+runPaths['main']+'\n\n')
-            if ('POWERREPORTS' in STAGES_TO_RUN):
-                summaryFile.write('                                                               Internal   Switching      Total     Leakage   \n')
-                summaryFile.write('                                                                  Power       Power      Power       Power   \n')
-                for mode in ['random', 'write', 'read', 'idle']:
-                    mode_power_report_file = open(PowerReportsPath +mode+'_mode_power.rpt','r')
-                    for line in mode_power_report_file:
-                        if 'Total (' in line:
-                            summaryFile.write('Total '+mode+'  \tmode power is :' +line + '\n')
-                        if ('Total Capacitance' in line) and (mode=='idle'):
-                            summaryFile.write('Total capacitance is :' +line + '\n')
-    
-            if ('PLACEANDROUTE' in STAGES_TO_RUN):     
-                total_wire_length = open(innoPath + 'reportWireLength.post_route.txt','r')               
-                for line in total_wire_length:
-                    if 'Total Wire Length' in line:
-                        summaryFile.write(line + '\n')
-
-                Density_file = open(innoPath + 'checkPlace.post_route.txt','r')
-                for line in Density_file:
-                    if 'Placement Density' in line:
-                        summaryFile.write(line + '\n')
-                timing_summary_file = open(innoPath + 'timing_summary.rpt','r')
-                for line in timing_summary_file:
-                    if 'Total' in line:
-                        summaryFile.write(line + '\n')
-
-                summaryFile.write('Place and Route run time is: ' + str(runTimes['PLACEANDROUTE']) + '\n')
-            summaryFile.write('Total run time is: ' + str(runTimes['TOTAL']) + '\n')
-            summaryFile.write('='*50+'\n')
-            summaryFile.close()
-            print('\n'+'#'*40)
-            print('FINISH CREATE REPORTS (summaryFile.txt)')
-            print('#'*40+'\n')
-
-
-"""
